0b0c7f4b2c888718647aea66c5e98f83b77692ff
CarlosGamezAscanio <131498406+CarlosGamezAscanio@users.noreply.github.com>
2024-05-18 22:37:58 -0400

    cv_mas_formulario

---
+++ controller/ContactosController.js
@@ -0,0 +1,29 @@
+const ContactosModel = require("../models/ContactosModel");
+
+class ContactosController {
+    constructor(){
+        this.contactosModel = new ContactosModel();
+        this.add = this.add.bind(this);
+    }
+
+    async add(req, res){
+        // Validar los datos del formulario
+        const { name, email, comment } = req.body;
+
+
+
+        //Guardar los datos del formulario
+        const ip = req.ip;
+        const fecha = new Date().toISOString();
+
+        await this.contactosModel.crearContacto(name, email, comment, ip, fecha);
+
+        const contactos = await this.contactosModel.obtenerAllContactos();
+    
+        console.log(contactos);
+
+        res.send("Formulario enviado con exito");
+    }
+}
+
+module.exports = ContactosController;
